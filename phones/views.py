2cf08ae45d054e6b125fcb1521de380450e27aa3
Александр Бугров <bugor9682@gmail.com>
2022-08-13 15:09:55 +0300

    part_1_done

--- phones/views.py
+++ phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,23 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort')
+    phones = Phone.objects.all()
+    if sort == 'min_price':
+        phones = phones.order_by('price')
+    elif sort == 'max_price':
+        phones = phones.order_by('-price')
+    elif sort == 'name':
+        phones = phones.order_by('-name')
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phones = Phone.objects.all()
+    for phone in phones:
+        if phone.slug == slug:
+            break
+    context = {'phone': phone}
     return render(request, template, context)
